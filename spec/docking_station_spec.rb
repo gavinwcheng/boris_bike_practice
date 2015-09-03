0f64b2d93b04d0af9348e703295a80745bb72e3a
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-09-04 00:08:25 +0100

    post challenge 1

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,24 @@
+require "docking_station"
+require "bike"
+
+describe DockingStation do
+  it "respond to release_bike" do
+    expect(subject).to respond_to :release_bike
+  end
+
+  it "releases working bike" do
+    #expect(subject.release_bike).to be_an_instance_of(Bike)
+    subject.dock Bike.new
+    expect(subject.release_bike).to be_working
+  end
+
+  it "respond to dock" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  describe "#release_bike" do
+    it "raises an error when there is no bikes available" do
+    expect { subject.release_bike }.to raise_error("No bikes available")
+    end
+  end
+end
