874ad4d3726472950d48bb5de4649a8d1e5c3dde
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-09-04 00:23:12 +0100

    post challenge 2_capacity 1

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,7 +18,14 @@ describe DockingStation do
 
   describe "#release_bike" do
     it "raises an error when there is no bikes available" do
-    expect { subject.release_bike }.to raise_error("No bikes available")
+      expect { subject.release_bike }.to raise_error("No bikes available")
+    end
+  end
+
+  describe "#dock" do
+    it "raises an error when docking station is full" do
+      subject.dock Bike.new
+      expect { subject.dock Bike.new }.to raise_error "Docking station full"
     end
   end
 end
